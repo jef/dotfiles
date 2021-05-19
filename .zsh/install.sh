b8db3195594d360099d47bb783c05a75197f1d4e
Jef LeCompte <jeffreylec@gmail.com>
2021-05-19 16:26:48 -0400

    feat: add install script

---
+++ .zsh/install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+_install_path="${HOME}/.dotfiles"
+_backup_path="${HOME}/.dotfiles-backup"
+git clone --bare https://raw.githubusercontent.com/jef/dotfiles/main/.zsh/install.sh "$_install_path"
+config() {
+   /usr/bin/git --git-dir="$_install_path" --work-tree="$HOME" "$@"
+}
+mkdir -p "$_backup_path"
+if config checkout; then
+  echo "Checked out config.";
+  else
+    echo "Backing up pre-existing dot files.";
+    config checkout 2>&1 | grep -E "\s+\." | awk {'print $1'} | xargs -I{} mv {} "$_backup_path/{}"
+fi;
+config checkout
+config config status.showUntrackedFiles no
