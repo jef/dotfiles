fe89a642898ae214db5b83c148ce150faf3d8a1f
Jef LeCompte <jeffreylec@gmail.com>
2021-05-19 16:37:53 -0400

    feat: add zgenom and asdf to install script

--- .zsh/install.sh
+++ .zsh/install.sh
@@ -8,10 +8,21 @@ config() {
 }
 mkdir -p "$_backup_path"
 if config checkout; then
-  echo "Checked out config.";
+  echo "Checked out dotfiles.";
   else
-    echo "Backing up pre-existing dot files.";
+    echo "Backing up pre-existing dotfiles.";
     config checkout 2>&1 | grep -E "\s+\." | awk {'print $1'} | xargs -I{} mv {} "$_backup_path/{}"
 fi;
 config checkout
 config config status.showUntrackedFiles no
+
+echo "Installing zgenom."
+
+git clone https://github.com/jandamm/zgenom.git "${HOME}/.zgenom"
+
+echo "Installing asdf."
+
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+if cd ~/.asdf; then
+    git checkout "$(git describe --abbrev=0 --tags)"
+fi
