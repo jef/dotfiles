84204c6055abd69b194a15893902ade8ba234563
Jef LeCompte <jeffreylec@gmail.com>
2021-06-25 10:07:58 -0700

    chore: update tools

--- .zsh/plugins/asdf.plugin.zsh
+++ .zsh/plugins/asdf.plugin.zsh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 ASDF_DIR="${ASDF_DIR:-$HOME/.asdf}"
-if [[ -d $ASDF_DIR ]]; then
-    source "${HOME}/.asdf/asdf.sh"
+if [[ -d "$ASDF_DIR" ]]; then
+    . "$HOME/.asdf/asdf.sh"
     fpath=(${ASDF_DIR}/completions $fpath)
 fi
