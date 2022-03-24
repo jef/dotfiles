0eb1127fbf24ef86f08b5a7fc547c5d0b0633491
Jef LeCompte <jeffreylec@gmail.com>
2022-03-24 11:21:54 -0700

    chore: remove keybindings

--- dot_zsh/runcom.sh
+++ dot_zsh/runcom.sh
@@ -10,10 +10,6 @@ fi
 # prompt theme
 source "$HOME/.zsh/themes/powerlevel10k.sh"
 
-# keybinds
-bindkey '^[[A' history-substring-search-up
-bindkey '^[[B' history-substring-search-down
-
 # completions
 zstyle ":completion:*" list-colors ${(s.:.)LS_COLORS}
 zstyle ":completion:*" use-cache on
