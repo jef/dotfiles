3972c128b318d929ca4b4c3de724077ef333c3cb
Jef LeCompte <jeffreylec@gmail.com>
2022-04-26 15:12:39 -0700

    chore: go back to p10k

--- dot_zsh/plugins.sh
+++ dot_zsh/plugins.sh
@@ -8,6 +8,7 @@ if ! zgenom saved; then
   zgenom load Aloxaf/fzf-tab
   zgenom load docker/cli contrib/completion/zsh
   zgenom load docker/compose contrib/completion/zsh 1.29.2
+  zgenom load romkatv/powerlevel10k powerlevel10k
   zgenom load zsh-users/zsh-completions
   zgenom load zsh-users/zsh-autosuggestions
   zgenom load zsh-users/zsh-history-substring-search
