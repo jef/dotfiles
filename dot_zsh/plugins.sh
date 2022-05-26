4ae19f5650ed340220946218e1678329c85f0f31
Jef LeCompte <jeffreylec@gmail.com>
2022-05-26 11:50:30 -0700

    feat: update completions

--- dot_zsh/plugins.sh
+++ dot_zsh/plugins.sh
@@ -9,6 +9,7 @@ if ! zgenom saved; then
   zgenom load docker/cli contrib/completion/zsh
   zgenom load docker/compose contrib/completion/zsh 1.29.2
   zgenom load romkatv/powerlevel10k powerlevel10k
+  zgenom load romkatv/zsh-prompt-benchmark
   zgenom load zsh-users/zsh-completions
   zgenom load zsh-users/zsh-autosuggestions
   zgenom load zsh-users/zsh-history-substring-search
