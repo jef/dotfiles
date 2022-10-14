f19707b42a8bf406d943ecc8d63237a26e816221
Jef LeCompte <jeffreylec@gmail.com>
2022-10-14 10:54:28 -0700

    style: update p10k

--- dot_zsh/plugins.sh
+++ dot_zsh/plugins.sh
@@ -6,17 +6,18 @@ if ! zgenom saved; then
   zgenom load "${HOME}/.zsh/completions"
 
   zgenom load Aloxaf/fzf-tab
+
   zgenom load docker/cli contrib/completion/zsh
   zgenom load docker/compose contrib/completion/zsh 1.29.2
+
   zgenom load romkatv/powerlevel10k powerlevel10k
   zgenom load romkatv/zsh-prompt-benchmark
+
   zgenom load zsh-users/zsh-completions
   zgenom load zsh-users/zsh-autosuggestions
   zgenom load zsh-users/zsh-history-substring-search
-  zgenom load zshzoo/keybindings
-  zgenom load zshzoo/setopts
 
-  zgenom load zsh-users/zsh-syntax-highlighting
+  zgenom load z-shell/F-Sy-H
 
   zgenom save
 fi
