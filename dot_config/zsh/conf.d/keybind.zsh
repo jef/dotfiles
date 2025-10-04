83fba056ae4c043390345e6c34e6462431c09916
Jef LeCompte <jeffreylec@gmail.com>
2025-10-03 21:53:34 -0700

    style: format

--- dot_config/zsh/conf.d/keybind.zsh
+++ dot_config/zsh/conf.d/keybind.zsh
@@ -6,6 +6,6 @@ bindkey '^[[B' history-substring-search-down
 bindkey "^[[1;3C" forward-word
 bindkey "^[[1;3D" backward-word
 
-bindkey  "^[[H"   beginning-of-line
-bindkey  "^[[F"   end-of-line
-bindkey  "^[[3~"  delete-char
+bindkey "^[[H" beginning-of-line
+bindkey "^[[F" end-of-line
+bindkey "^[[3~" delete-char
