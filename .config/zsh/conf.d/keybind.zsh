519a44be7e3355051c2fefa7de86f85868edd718
Jef LeCompte <jeffreylec@gmail.com>
2026-03-24 22:08:14 -0700

    feat: more binds, git-stash alias, python env

--- .config/zsh/conf.d/keybind.zsh
+++ .config/zsh/conf.d/keybind.zsh
@@ -5,6 +5,10 @@ bindkey '^[[B' history-substring-search-down
 
 bindkey "^[[1;3C" forward-word
 bindkey "^[[1;3D" backward-word
+bindkey "^[OC" forward-word
+bindkey "^[OD" backward-word
+bindkey "^[f" forward-word
+bindkey "^[b" backward-word
 
 bindkey "^[[H" beginning-of-line
 bindkey "^[[F" end-of-line
